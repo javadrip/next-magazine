43c356cd763f13d7a5cbeafd215a1e82c2540ee6
Vinleon Ang <javadripdev@gmail.com>
2023-04-19 13:11:05 +0800

    Move project type to sanity-utils

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { getProjects } from "@/sanity/sanity-utils";
-import { Project } from "@/types/Project";
 
 export default async function Home() {
   const projects = await getProjects();
 
   return (
     <div>
-      {projects.map((project: Project) => (
+      {/* projects and project are all typed correctly here */}
+      {projects.map(project => (
         <div key={project._id}>
           {project.name} ID: {project._id}
         </div>
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,7 +1,11 @@
+// Generally a good practice to keep all types in sanity-utils.ts
+
+import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 
 // Functions that are used to grab data lives here
-export async function getProjects() {
+// Typing getProjects here ensures that everywhere that uses getProjects will get typed appropriately
+export async function getProjects(): Promise<Project[]> {
   const client = createClient({
     projectId: "p8qypvm2",
     dataset: "production",
