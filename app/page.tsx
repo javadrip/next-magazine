f1242520b0abc1e5befbded7cfb03b1359b95f8c
Vinleon Ang <javadripdev@gmail.com>
2023-04-19 12:58:56 +0800

    Add type to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import { Project } from "@/types/Project";
 
 export default async function Home() {
   const projects = await getProjects();
 
   return (
     <div>
-      {projects.map(project => (
-        <div key={project.id}>
+      {projects.map((project: Project) => (
+        <div key={project._id}>
           {project.name} ID: {project._id}
         </div>
       ))}
