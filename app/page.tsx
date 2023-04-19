d26b47fded64402072f4bd81543535c73f13e360
Vinleon Ang <javadripdev@gmail.com>
2023-04-19 16:17:49 +0800

    Add basic styling and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,46 @@
 import { getProjects } from "@/sanity/sanity-utils";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
 
   return (
-    <div>
+    <div className="max-w-5xl mx-auto py-20">
+      <h1 className="text-7xl font-extrabold">
+        Hello, I&apos;m{" "}
+        <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+          Vin
+        </span>
+        !
+      </h1>
+      <p className="mt-3 text-xl text-gray-600">
+        Aloha everyone! Check out my projects!
+      </p>
+
+      <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
       {/* projects and project are all typed correctly here */}
-      {projects.map(project => (
-        <div key={project._id}>
-          {project.name} ID: {project._id}
-        </div>
-      ))}
+      <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {projects.map(project => (
+          <div
+            key={project._id}
+            className="border border-gray-500 rounded-lg p-3"
+          >
+            <div className="font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+              {project.image && (
+                <Image
+                  src={project.image}
+                  alt={project.name}
+                  width={300}
+                  height={300}
+                  className="object-cover rounded-lg border border-gray-500"
+                />
+              )}
+              {project.name}
+            </div>
+            {project.name} ID: {project._id}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
