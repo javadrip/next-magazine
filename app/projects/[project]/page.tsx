493983bf4623d68af90be8cf230c5559e2b2d6f2
Vinleon Ang <javadripdev@gmail.com>
2023-04-20 11:58:06 +0800

    Connect project data to dynamic pages

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -1,8 +1,22 @@
+import { getProject } from "@/sanity/sanity-utils";
+
 type Props = {
   params: { project: string };
 };
 
+// Next automatically passes the route to params
 export default async function Project({ params }: Props) {
+  // params is an object with just one key of project
+  // The key "project" comes from the dynamic route file name [project]
   const slug = params.project;
+  // Type of getProject is defined in sanity-utils.ts
   const project = await getProject(slug);
+
+  console.log(params);
+
+  return (
+    <div>
+      {project.name} | {params.project}
+    </div>
+  );
 }
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,9 +1,9 @@
+// Functions that are used to grab data lives here
 // Generally a good practice to keep all types in sanity-utils.ts
 
 import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 
-// Functions that are used to grab data lives here
 // Typing getProjects here ensures that everywhere that uses getProjects will get typed appropriately
 export async function getProjects(): Promise<Project[]> {
   const client = createClient({
