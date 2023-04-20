bee6ccdc3fcc7c494583410e36790bffac202310
Vinleon Ang <javadripdev@gmail.com>
2023-04-20 18:09:27 +0800

    Add styles to page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -12,5 +12,24 @@ export default async function Project({ params }: Props) {
   // Type of getProject is defined in sanity-utils.ts
   const project = await getProject(slug);
 
-  return <div>{project.name}</div>;
+  return (
+    <div className="max-w-3xl mx-auto py-20">
+      <header className="flex items-center justify-between">
+        <h1 className="text-5xl font-extrabold drop-shadow bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+          {project.name}
+        </h1>
+        <a
+          href={project.url}
+          title="View Project"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-pink-500 hover:text-pink-100 transition"
+        >
+          View Project
+        </a>
+      </header>
+
+      {/* We can't just display content using {project.content} because it's an array of blocks */}
+    </div>
+  );
 }
