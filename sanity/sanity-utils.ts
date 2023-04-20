7038b8e40d2a435ea97fa05e203c6d9a4f5834f5
Vinleon Ang <javadripdev@gmail.com>
2023-04-20 22:37:23 +0800

    Add function to query data for pages

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,11 +1,13 @@
 // Functions that are used to grab data lives here
 // Generally a good practice to keep all types in sanity-utils.ts
 
-import { Project } from "@/types/Project";
 import { createClient, groq } from "next-sanity";
 import clientConfig from "./config/client-config";
+import { Project } from "@/types/Project";
+import { Page } from "@/types/Page";
 
 // Typing getProjects here ensures that everywhere that uses getProjects will get typed appropriately
+// Used to display a list of projects
 export async function getProjects(): Promise<Project[]> {
   // clientConfig is imported from sanity/config/client-config.ts
   return createClient(clientConfig).fetch(
@@ -24,6 +26,7 @@ export async function getProjects(): Promise<Project[]> {
   );
 }
 
+// Used to display the content of a single project page
 export async function getProject(slug: string): Promise<Project> {
   // clientConfig is imported from sanity/config/client-config.ts
   return createClient(clientConfig).fetch(
@@ -43,5 +46,32 @@ export async function getProject(slug: string): Promise<Project> {
     // Short hand for { slug: slug }
     // slug value is taken from the getProject parameter slug
     { slug }
+  );
+}
+
+// Used to display a list of pages
+export async function getPages(): Promise<Page[]> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page"]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+    }`
+  );
+}
+
+// Used to display the content of a page
+export async function getPage(slug: string): Promise<Page> {
+  return createClient(clientConfig).fetch(
+    groq`*[_type == "page" && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      "slug": slug.current,
+      content
+    },
+    { slug }
+    `
   );
 }
