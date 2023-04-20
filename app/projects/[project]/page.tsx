5db33f043a36b6d12998feeaae49cef3bba09393
Vinleon Ang <javadripdev@gmail.com>
2023-04-20 19:24:46 +0800

    Create basic navbar

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -15,7 +15,7 @@ export default async function Project({ params }: Props) {
   const project = await getProject(slug);
 
   return (
-    <div className="max-w-3xl mx-auto py-20">
+    <div>
       <header className="flex items-center justify-between">
         <h1 className="text-5xl font-extrabold drop-shadow bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
           {project.name}
