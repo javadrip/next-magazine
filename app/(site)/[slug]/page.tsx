2f255cc3c2daad5969196e945b2a5d0b3c6c11f2
Vinleon Ang <javadripdev@gmail.com>
2023-04-20 23:30:30 +0800

    Ad style to page

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getPage } from "@/sanity/sanity-utils";
+import { PortableText } from "@portabletext/react";
 
 type Props = {
   params: {
@@ -8,5 +9,15 @@ type Props = {
 
 export default async function Page({ params }: Props) {
   const page = await getPage(params.slug);
-  return <div>{page.title}</div>;
+  return (
+    <div>
+      <h1 className="text-5xl font-extrabold drop-shadow bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
+        {page.title}
+      </h1>
+
+      <div className="text-lg text-gray-700 mt-10">
+        <PortableText value={page.content} />
+      </div>
+    </div>
+  );
 }
