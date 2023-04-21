7204b83c979e62057bb5e694f6ce46de68561a00
Vinleon Ang <javadripdev@gmail.com>
2023-04-21 18:06:10 +0800

    Rearrange imports and exports

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,15 +1,15 @@
+import author from "./author";
 import blockContent from "./blockContent";
 import category from "./category";
-import post from "./post";
-import author from "./author";
-import project from "./project-schema";
 import page from "./page-schema";
+import post from "./post";
+import project from "./project-schema";
 
 export const schemaTypes = [
-  post,
   author,
-  category,
   blockContent,
-  project,
+  category,
   page,
+  post,
+  project,
 ];
