fe52866f4691d92a284c830eb4869f979ad05d0a
Vinleon Ang <javadripdev@gmail.com>
2023-04-21 17:24:35 +0800

    Remove highlight, superscript and subscript decorators

--- sanity/schemas/blockContent.ts
+++ sanity/schemas/blockContent.ts
@@ -30,7 +30,10 @@ export default defineType({
         { title: "H4", value: "h4" },
         { title: "Quote", value: "blockquote" },
       ],
-      lists: [{ title: "Bullet", value: "bullet" }],
+      lists: [
+        { title: "Bullet", value: "bullet" },
+        { title: "Numbered", value: "number" },
+      ],
       // Marks let you mark up inline text in the block editor.
       marks: {
         // Decorators usually describe a single property – e.g. a typographic
@@ -41,7 +44,10 @@ export default defineType({
           { title: "Underline", value: "underline" },
           { title: "Strike", value: "strike-through" },
           { title: "Code", value: "code" },
-          { title: "Highlight", value: "highlight" },
+          {
+            title: "Highlight",
+            value: "highlight",
+          },
           { title: "Subscript", value: "subscript" },
           { title: "Superscript", value: "superscript" },
         ],
